9c065a7fd004c4aaf883fa84e5aa792997be44b2
Wojciech Grabowski (PGS Software) <wgrabowski@pgs-soft.com>
2016-07-21 19:15:50 +0200

    Initial commit

---
+++ jirolog.sh
@@ -0,0 +1,7 @@
+#!/bin/env bash
+CONFIG_PATH='config.properties'
+source $CONFIG_PATH;
+echo $JIRA_USERNAME
+#URL="https://${JIRA_HOSTNAME}/rest/api/2/search?jql=status+%21%3D+Closed+AND+status+%21%3D+Resolved+AND+assignee+%3D+${JIRA_USERNAME}"
+URL="https://${JIRA_HOSTNAME}/rest/agile/latest/board/34/issue?jql=updated%20%3E%20startOfDay()%20and%20updated%20%3C%20endOfDay()&fields=worklogs"
+curl -D- -u ${JIRA_USERNAME}:${JIRA_PASSWORD} -X GET -H 'Content-Type: application/json' "$URL"
