693d46ec86df81af8713023e15d9ba738ecc80d2
Wojciech Grabowski <wojtek@wojciech-grabowski.pl>
2016-07-25 10:00:49 +0200

    Working CURL to JIRA

--- jirolog.sh
+++ jirolog.sh
@@ -1,7 +1,8 @@
 #!/bin/env bash
-CONFIG_PATH='config.properties'
+CONFIG_PATH='jira.properties'
 source $CONFIG_PATH;
 echo $JIRA_USERNAME
 #URL="https://${JIRA_HOSTNAME}/rest/api/2/search?jql=status+%21%3D+Closed+AND+status+%21%3D+Resolved+AND+assignee+%3D+${JIRA_USERNAME}"
 URL="https://${JIRA_HOSTNAME}/rest/agile/latest/board/34/issue?jql=updated%20%3E%20startOfDay()%20and%20updated%20%3C%20endOfDay()&fields=worklogs"
-curl -D- -u ${JIRA_USERNAME}:${JIRA_PASSWORD} -X GET -H 'Content-Type: application/json' "$URL"
+echo "curl -D- -u ${JIRA_USERNAME}:${JIRA_PASSWORD} -X GET -H 'Content-Type: application/json' --insecure $URL"
+curl -D- -u "${JIRA_USERNAME}:${JIRA_PASSWORD}" -X GET -H 'Content-Type: application/json' --insecure "$URL"
