729c3d6c33ddf792ed04c2cbaa9c082db12c343e
Wojciech Grabowski <wojtek@wojciech-grabowski.pl>
2016-07-26 17:29:22 +0200

    parser: work in progress

--- jirolog.sh
+++ jirolog.sh
@@ -1,8 +1,9 @@
 #!/bin/env bash
 CONFIG_PATH='jira.properties'
 source $CONFIG_PATH;
-echo $JIRA_USERNAME
 #URL="https://${JIRA_HOSTNAME}/rest/api/2/search?jql=status+%21%3D+Closed+AND+status+%21%3D+Resolved+AND+assignee+%3D+${JIRA_USERNAME}"
-URL="https://${JIRA_HOSTNAME}/rest/agile/latest/board/34/issue?jql=updated%20%3E%20startOfDay()%20and%20updated%20%3C%20endOfDay()&fields=worklogs"
-echo "curl -D- -u ${JIRA_USERNAME}:${JIRA_PASSWORD} -X GET -H 'Content-Type: application/json' --insecure $URL"
-curl -D- -u "${JIRA_USERNAME}:${JIRA_PASSWORD}" -X GET -H 'Content-Type: application/json' --insecure "$URL"
+URL="https://${JIRA_HOSTNAME}/rest/agile/latest/board/34/issue?jql=updated%20%3E%20startOfDay()%20and%20updated%20%3C%20endOfDay()&fields=worklog"
+curl -D- -s -u "${JIRA_USERNAME}:${JIRA_PASSWORD}" -X GET -H 'Content-Type: application/json' --insecure "$URL" -o data.json >/dev/null
+echo "Amount Work logged by $JIRA_USERNAME in $JIRA_HOSTNAME today:";
+
+./parse.js "${JIRA_USERNAME}"
