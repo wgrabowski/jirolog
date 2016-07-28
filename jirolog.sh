05e8962c9916d6be56c16601f0fdca79b2ee95f3
Wojciech Grabowski <wojtek@wojciech-grabowski.pl>
2016-07-28 20:02:49 +0200

    Ugly but working example

--- jirolog.sh
+++ jirolog.sh
@@ -1,9 +1,16 @@
 #!/bin/env bash
 CONFIG_PATH='jira.properties'
 source $CONFIG_PATH;
+FROM_DATE=`date +"%Y-%m-%d"`;
+FROM_DATE="$1";
+
 #URL="https://${JIRA_HOSTNAME}/rest/api/2/search?jql=status+%21%3D+Closed+AND+status+%21%3D+Resolved+AND+assignee+%3D+${JIRA_USERNAME}"
-URL="https://${JIRA_HOSTNAME}/rest/agile/latest/board/34/issue?jql=updated%20%3E%20startOfDay()%20and%20updated%20%3C%20endOfDay()&fields=worklog"
+#JQL_QUERY="updated%20>=%20%27$FROM_DATE%2000:00%27%20and%20updated%20<=%20%27$FROM_DATE%2023:59%27&fields=summary,worklog"
+JQL_QUERY="timespent%20>%200%20ORDER%20BY%20updatedDate&fields=summary,worklog"
+URL="https://${JIRA_HOSTNAME}/rest/agile/latest/board/34/issue?jql=$JQL_QUERY"
+echo "$URL"
 curl -D- -s -u "${JIRA_USERNAME}:${JIRA_PASSWORD}" -X GET -H 'Content-Type: application/json' --insecure "$URL" -o data.json >/dev/null
+echo "JIROLOG"
 echo "Amount Work logged by $JIRA_USERNAME in $JIRA_HOSTNAME today:";
 
-./parse.js "${JIRA_USERNAME}"
+./parse.js "${JIRA_USERNAME}" "$FROM_DATE";
--- parse.js
+++ parse.js
@@ -4,32 +4,44 @@ var data = require('./data.json');
 var jiraUsername = process.env.JIRA_USER
 
 var userName = process.argv[2] || "";
-var day = new Date(process.argv[3]) || "2016-07-26;
-
-
+var day = new Date(process.argv[3]) || new Date("2016-07-26");
+function rightPad(str, len,separator=" ") {
+    let pad = Array(len).join(separator);
+    return (pad + str).slice(str.length)
+}
 function getUserWorklogs(userName) {
     var issues = data.issues;
     var lastUpdatedIssues = issues.map(i=> {
-        return {key: i.key, worklogs: i.fields.worklog.worklogs}
+        return {key: i.key, summary: i.fields.summary, worklogs: i.fields.worklog.worklogs}
     });
 
     var issuesWithUserWorklog = lastUpdatedIssues.map(i=> {
-        return {issueKey: i.key, worklogs: i.worklogs.filter(w => w.author.name === userName)}
+        return {issueKey: i.key, summary: i.summary, worklogs: i.worklogs.filter(w => w.author.name === userName)}
     }).filter(i=>i.worklogs.length > 0);
-    issuesWithUserWorklog.forEach(i=>i.worklogs.filter(w=>isFromDay(w,day)))
+
+    issuesWithUserWorklog.forEach(i=>i.worklogs = i.worklogs.filter(w=>isFromDay(w, day)));
+    issuesWithUserWorklog = issuesWithUserWorklog.filter(i=>i.worklogs.length > 0);
     return issuesWithUserWorklog
 }
-function isFromDay(worklog,day){
+function isFromDay(worklog, day) {
+    let nextDay = new Date(day.getTime() + 86400000);
     let worklogStarted = new Date(worklog.started);
-    return worklogStarted.toTimeString() >= day.toTimeString()
+    return (worklogStarted.getTime() >= day.getTime()) && (worklogStarted.getTime() < nextDay.getTime());
 }
 
-function printOutput(data){
-    data.forEach(d=>console.log(d));
+function printOutput(data) {
+    data.forEach(d=>{console.log(formatIsse(d));console.log(rightPad("+",80,"-"));});
 }
-
+function formatIsse(issue) {
+    return `${rightPad(issue.issueKey, 6)}: ${rightPad(issue.summary.slice(0, 30), 30)} ${formatWorklogs(issue.worklogs)}`
+}
+function formatWorklogs(worklogs) {
+    let output = `\t`;
+    output += worklogs.map(worklog=>`${worklog.timeSpent} ${worklog.comment}`);
+    return output
+}
 var worklogs = getUserWorklogs(userName);
 printOutput(worklogs);
 
 
-process.stdout.write("kupaaa\n");
+
