27947c6795bb601d75231e0ac2d1163654b9df47
Wojciech Grabowski <wojtek@wojciech-grabowski.pl>
2016-08-04 19:53:43 +0200

    user workflow

--- jirolog.sh
+++ jirolog.sh
@@ -1,16 +1,48 @@
 #!/bin/env bash
-CONFIG_PATH='jira.properties'
+JIROLOG_HOME=$(dirname $0);
+CONFIG_PATH=$JIROLOG_HOME'/jira.properties'
+echo $JIROLOG_HOME
+JIROLOG_PARSER="$JIROLOG_HOME/parse.js"
 source $CONFIG_PATH;
-FROM_DATE=`date +"%Y-%m-%d"`;
-FROM_DATE="$1";
+DAY_LABEL="today";
+DATE_TODAY=`date +"%Y-%m-%d"`;
+DATE_YESTERDAY=`date -d "12 hours ago" '+%Y-%m-%d'`
 
-#URL="https://${JIRA_HOSTNAME}/rest/api/2/search?jql=status+%21%3D+Closed+AND+status+%21%3D+Resolved+AND+assignee+%3D+${JIRA_USERNAME}"
-#JQL_QUERY="updated%20>=%20%27$FROM_DATE%2000:00%27%20and%20updated%20<=%20%27$FROM_DATE%2023:59%27&fields=summary,worklog"
+get_logs_from_date(){
 JQL_QUERY="timespent%20>%200%20ORDER%20BY%20updatedDate&fields=summary,worklog"
-URL="https://${JIRA_HOSTNAME}/rest/agile/latest/board/34/issue?jql=$JQL_QUERY"
-echo "$URL"
+URL="https://${JIRA_HOSTNAME}/rest/agile/latest/board/$JIRA_BOARD_ID/issue?jql=$JQL_QUERY"
 curl -D- -s -u "${JIRA_USERNAME}:${JIRA_PASSWORD}" -X GET -H 'Content-Type: application/json' --insecure "$URL" -o data.json >/dev/null
 echo "JIROLOG"
-echo "Amount Work logged by $JIRA_USERNAME in $JIRA_HOSTNAME today:";
+echo "Amount Work logged by $JIRA_USERNAME in $JIRA_HOSTNAME $DAY_LABEL:";
 
-./parse.js "${JIRA_USERNAME}" "$FROM_DATE";
+"$JIROLOG_PARSER" "${JIRA_USERNAME}" "$1";
+}
+
+USAGE="Usage: $0 [--today | -t | --yesterday | -y| --date <date>"
+
+if [ "$#" == "0" ]; then
+	echo "$USAGE"
+	exit 1
+fi
+
+while (( "$#" )); do
+
+case $1 in
+    --today|-t)
+       get_logs_from_date "$DATE_TODAY"
+    ;;
+    --yesterday | -y)
+        get_logs_from_date "$DATE_YESTERDAY"
+        ;;
+    --date | -d)
+    get_logs_from_date "$2"
+    shift
+    ;;
+    *)
+    echo "$USAGE"
+    shift
+    ;;
+    esac
+shift
+
+done
